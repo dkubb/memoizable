df1d3528300e0e0d97d8baa3956c0eb58b93a9a3
Dan Kubb <dan.kubb@gmail.com>
2013-10-05 11:14:21 -0700

    Refactor memoized instance method creation to be simpler

--- lib/idem/module_methods.rb
+++ lib/idem/module_methods.rb
@@ -127,7 +127,7 @@ module Idem
       name = method.name.to_sym
       undef_method(name)
       define_method(name) do ||
-        memoized_method_cache.fetch(name) { method.bind(self).call }
+        memoized_method_cache.fetch(name, &method.bind(self))
       end
     end
 
