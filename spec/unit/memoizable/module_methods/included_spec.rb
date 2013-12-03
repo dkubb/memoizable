742acf57d6a9002d81dbbcd119962a7dd3c0d430
Dan Kubb <dan.kubb@gmail.com>
2013-12-02 21:34:07 -0800

    Refactor spec example to use a shared spec

--- spec/unit/memoizable/module_methods/included_spec.rb
+++ spec/unit/memoizable/module_methods/included_spec.rb
@@ -14,24 +14,7 @@ describe Memoizable::ModuleMethods, '#included' do
     Memoizable.stub(:included)
   end
 
-  around do |example|
-    # Restore included method after each example
-    superclass.class_eval do
-      alias_method :original_included, :included
-      example.call
-      undef_method :included
-      alias_method :included, :original_included
-    end
-  end
-
-  it 'delegates to the superclass #included method' do
-    # This is the most succinct approach I could think of to test whether the
-    # superclass#included method is called. All of the built-in rspec helpers
-    # did not seem to work for this.
-    included = false
-    superclass.class_eval { define_method(:included) { |_| included = true } }
-    expect { subject }.to change { included }.from(false).to(true)
-  end
+  it_behaves_like 'it calls super', :included
 
   it 'includes Memoizable into the descendant' do
     subject
