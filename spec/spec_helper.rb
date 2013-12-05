95213961a48a6457164259f13c2fcfa748231f0c
Dan Kubb <dan.kubb@gmail.com>
2013-12-04 20:06:22 -0800

    Change spec helper to use devtools style organization

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,27 @@
 # encoding: utf-8
 
-require 'simplecov'
-require 'coveralls'
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start do
-  command_name 'spec'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
 
-  add_filter 'config'
-  add_filter 'spec'
-  add_filter 'vendor'
+  SimpleCov.start do
+    command_name 'spec:unit'
 
-  minimum_coverage 100
+    add_filter 'config'
+    add_filter 'spec'
+    add_filter 'vendor'
+
+    minimum_coverage 100
+  end
 end
 
+require 'devtools/spec_helper'
 require 'memoizable'
-require 'rspec'
-
-# Require spec support files and shared behavior
-Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each do |file|
-  require file.chomp('.rb')
-end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expect_with|
