6d0792198ae7fb54ed7daf19616278e20040c5f9
Tim Liner <trliner@gmail.com>
2013-12-20 17:19:29 -0600

    Add integration test for deserialization with Marshal

--- spec/integration/serializable_spec.rb
+++ spec/integration/serializable_spec.rb
@@ -4,7 +4,10 @@ require 'spec_helper'
 
 class Serializable
   include Memoizable
-  def method; end
+
+  def method
+    rand(10000)
+  end
   memoize :method
 end
 
@@ -12,8 +15,20 @@ describe 'A serializable object' do
   let(:serializable) do
     Serializable.new
   end
-  it 'is serializable with Marshal' do
+
+  before do
     serializable.method # Call the method to trigger lazy memoization
+  end
+
+  it 'is serializable with Marshal' do
     expect { Marshal.dump(serializable) }.not_to raise_error
+  end
+
+  it 'is deserializable with Marshal' do
+    serialized = Marshal.dump(serializable)
+    deserialized = Marshal.load(serialized)
+
+    expect(deserialized).to be_an_instance_of(Serializable)
+    expect(deserialized.method).to eql(serializable.method)
   end
 end
