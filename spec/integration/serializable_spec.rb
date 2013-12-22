be5db41f5fbf610588026b230878dc7b1a13d541
Tim Liner <trliner@gmail.com>
2013-12-22 14:14:50 -0600

    Rename #method method in Serializable spec

--- spec/integration/serializable_spec.rb
+++ spec/integration/serializable_spec.rb
@@ -5,10 +5,10 @@ require 'spec_helper'
 class Serializable
   include Memoizable
 
-  def method
+  def random_number
     rand(10000)
   end
-  memoize :method
+  memoize :random_number
 end
 
 describe 'A serializable object' do
@@ -17,7 +17,7 @@ describe 'A serializable object' do
   end
 
   before do
-    serializable.method # Call the method to trigger lazy memoization
+    serializable.random_number # Call the memoized method to trigger lazy memoization
   end
 
   it 'is serializable with Marshal' do
@@ -29,6 +29,6 @@ describe 'A serializable object' do
     deserialized = Marshal.load(serialized)
 
     expect(deserialized).to be_an_instance_of(Serializable)
-    expect(deserialized.method).to eql(serializable.method)
+    expect(deserialized.random_number).to eql(serializable.random_number)
   end
 end
